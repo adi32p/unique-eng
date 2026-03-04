9963e644254e0ff4b27ae786dbeb929a616e9787
adi32p <aditya.22210268@viit.ac.in>
2026-03-04 19:30:44 +0530

    Fix CORS for Vercel frontend

--- backend/app.js
+++ backend/app.js
@@ -16,7 +16,8 @@ const app = express();
 // Middlewares
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:8080"],
+    origin: ["http://localhost:5173", "http://localhost:8080","https://uniqueeng.vercel.app"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
 );
