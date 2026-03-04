75f01609ff052f2a6b74cdc67379cdb98fc6b99c
adi32p <aditya.22210268@viit.ac.in>
2026-03-04 21:57:17 +0530

    done

--- backend/utils/mailer.js
+++ backend/utils/mailer.js
@@ -1,4 +1,6 @@
 const nodemailer = require("nodemailer");
+console.log("EMAIL_USER:", process.env.EMAIL_USER);
+console.log("EMAIL_PASS exists:", !!process.env.EMAIL_PASS);
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
