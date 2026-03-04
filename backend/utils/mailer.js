25a7ca5d02421ab3a013e65aed0a90a7501a8752
adi32p <aditya.22210268@viit.ac.in>
2026-03-04 21:36:47 +0530

    done

--- backend/utils/mailer.js
+++ backend/utils/mailer.js
@@ -11,10 +11,19 @@ const transporter = nodemailer.createTransport({
 });
 
 exports.sendEmail = async (to, subject, htmlContent) => {
-  await transporter.sendMail({
-    from: `"Unique EPC" <${process.env.EMAIL_USER}>`,
-    to,
-    subject,
-    html: htmlContent, 
-  });
+  try {
+    console.log("Sending email to:", to);
+
+    const info = await transporter.sendMail({
+      from: `"Unique EPC" <${process.env.EMAIL_USER}>`,
+      to,
+      subject,
+      html: htmlContent,
+    });
+
+    console.log("Email sent:", info.messageId);
+  } catch (error) {
+    console.error("EMAIL ERROR:", error);
+    throw error;
+  }
 };
